977d08c39219ba74c3ea976893a0911efc38e4bc
10banov <info@atlant2010.ru>
2023-01-22 23:10:23 +0300

    cli script modiff

--- atl-scan-cli.php
+++ atl-scan-cli.php
@@ -6,70 +6,77 @@ define("NOT_CHECK_PERMISSIONS",true);
 define("BX_CRONTAB", true);
 define("BX_NO_ACCELERATOR_RESET", true);
 
+$cliParams = getopt(null, ["monitorio-key:"]);
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+use \Bitrix\Main\Web\HttpClient;
+
+if (function_exists('mb_internal_encoding'))
+    mb_internal_encoding('ISO-8859-1');
 
 CModule::IncludeModule('bitrix.xscan');
 
 set_time_limit(0);
 
 ini_set('memory_limit', '10000M');
-//var_dump(ini_get('memory_limit'));exit;
 
-//IncludeModuleLangFile(__FILE__);
+$progress = 0;
+$total = 0;
+$inprogress = False;
 
-if (function_exists('mb_internal_encoding'))
-    mb_internal_encoding('ISO-8859-1');
+$start_path = $_SERVER['DOCUMENT_ROOT'];
+$start_path = rtrim($start_path, '/');
 
-define('XSCAN_LOG', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/bitrix.xscan/file_list_cli.txt');
-define('XSCAN_DB_LOG', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/bitrix.xscan/db_list_cli.txt');
-define('XSCAN_DATABASE', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/bitrix.xscan/database.json');
+$scaner = new CBitrixXscan($progress, $total);
 
-define('ATL_XSCAN_LOG_IGNORE', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/bitrix.xscan/file_list_ignore.txt');
+var_dump($scaner->doc_root);
 
-//define('START_TIME', time()); // засекаем время старта
-define('START_TIME', PHP_INT_MAX); // ставим время старта заранее большое чтобы анализ в CLI-режиме не завершался по таймауту
+$scaner->scan_log =  $scaner->doc_root. '/bitrix/modules/bitrix.xscan/file_list_cli.txt';
+$scaner->start_time = PHP_INT_MAX;
 
-$strError = '';
-
-$START_PATH = $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/';
-
-if (!$START_PATH)
-    $START_PATH = $_SERVER['DOCUMENT_ROOT'];
-
-/*if (file_exists(XSCAN_LOG))
-    unlink(XSCAN_LOG);*/
-
-/*if (file_exists(XSCAN_DB_LOG))
-    unlink(XSCAN_DB_LOG);*/
-
-//CBitrixXscan::CheckEvents();
-//CBitrixXscan::CheckAgents();
-
-var_dump($START_PATH);
-
-//CBitrixXscan::Search($START_PATH); 
+//$scaner->clean(); // new scan
+$scaner->CheckEvents();
+$scaner->CheckAgents();
+$scaner->Count_total($start_path);
+$scaner->Search($start_path);
 
 
-
-
-// @TODO: Надо сделать исключение из результатов XSCAN_DB_LOG те строки, которые соответствуют 1в1 строкам из file_list_cli.ignore
+// Обработка исключений:
 // file_list_cli.ignore
 // И результативный список с учетом исключений записать в file_list.txt
-$ignorelist = implode(" #### ", file(ATL_XSCAN_LOG_IGNORE));
-var_dump($ignorelist);
+$ignorelist = implode(" #### ", file(__DIR__ . '/file_list_ignore.txt'));
+//var_dump($ignorelist);
 //file
-$productionLog = str_replace(basename(XSCAN_LOG), "file_list.txt", XSCAN_LOG);
+$productionLog = str_replace(basename($scaner->scan_log), "file_list.txt", $scaner->scan_log);
 file_put_contents($productionLog, "");
-$fp = @fopen(XSCAN_LOG, "r");
+$fp = @fopen($scaner->scan_log, "r");
 if ($fp) {
     while (($row = fgets($fp)) !== false) {
         $row = trim($row);
         //var_dump($row);
         // preg_match("#$row#ium", $ignorelist)
         if (stripos($ignorelist, $row)===false) {
-            //echo $row."\n";
+            echo "[LISTED] ".$row."\n";
             file_put_contents($productionLog, $row."\n", FILE_APPEND);
+        }
+        else {
+            echo "[IGNORED] ".$row."\n";
         }
     }
     fclose($fp);
 }
+
+if (filesize($productionLog)>0 && !empty($cliParams['monitorio-key'])) {
+    $httpClient = new HttpClient();
+    
+    $httpClient->post('https://monitorio.io/api/notifications/add/', json_encode([
+        'key' => $cliParams['monitorio-key'],
+        'tpl' => 'malware',
+        'params' => [
+            'list' => file_get_contents($productionLog),
+        ],
+    ]));
+}
+
+echo "DONE\n";
